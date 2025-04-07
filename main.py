e34bd04f105eab7bd339e692e634d97310bcf5fc
Hannibal <lenesis@proton.me>
2025-04-07 16:46:33 +0330

    gathering all

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+from modules import find_vid,get_length,parser
+parser_arguments = parser.get_args()
+
+files_list = find_vid.search(
+    parser_arguments['target_dir']
+    , parser_arguments['sub_dir']
+    , parser_arguments['types']
+    )
+
+get_length.give_me_the_lengths(files_list)
