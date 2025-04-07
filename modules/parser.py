b565ef408c6e10036abe1f51750e16916cce1fc5
Hannibal <lenesis@proton.me>
2025-04-07 15:35:23 +0330

    changed to a function

--- modules/parser.py
+++ modules/parser.py
@@ -1,35 +1,36 @@
 import argparse
 
-# parser defined here , 
+def get_args() -> dict:
+    # parser defined here , 
 
-parser = argparse.ArgumentParser(
-    prog='glenth2'
-    , description='glenth2 helps you find out total length of videos'
-    , usage='glenth2 [options]'
-    , epilog='for more information visit : https://gtihub.com/thehannibalist/glenth2'
-)
+    parser = argparse.ArgumentParser(
+        prog='glenth2'
+        , description='glenth2 helps you find out total length of videos'
+        , usage='glenth2 [options]'
+        , epilog='for more information visit : https://gtihub.com/thehannibalist/glenth2'
+    )
 
-# custom directory option
-parser.add_argument(
-    '-d'
-    , '--target-directory'
-    , default='.'
-    , help='choose a directory to search for videos (default is current directory.)'
-)
+    # custom directory option
+    parser.add_argument(
+        '-d'
+        , '--target-directory'
+        , default='.'
+        , help='choose a directory to search for videos (default is current directory.)'
+    )
 
-# show version option
-parser.add_argument(
-    '-v'
-    , '--version'
-    , help='prints current version and exits'
-    , action='store_true'
-)
+    # show version option
+    parser.add_argument(
+        '-v'
+        , '--version'
+        , help='prints current version and exits'
+        , action='store_true'
+    )
 
-# choose types option
-parser.add_argument (
-    '-t'
-    , '--types'
-    , default='all'
-    , help='video extensions to search for (seperate with commas (e.g. mp4,mkv)) (default is all possible videos)'
-)
+    # choose types option
+    parser.add_argument (
+        '-t'
+        , '--types'
+        , default='all'
+        , help='video extensions to search for (seperate with commas (e.g. mp4,mkv)) (default is all possible videos)'
+    )
 
