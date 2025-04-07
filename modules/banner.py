e115a101d86bf43a06072571292ddd6f20baef96
Hannibal <lenesis@proton.me>
2025-04-07 17:39:11 +0330

    update banner

--- modules/banner.py
+++ modules/banner.py
@@ -2,16 +2,17 @@ from time import sleep
 
 def show():
     logo =[
-        '    █ ▗▞▀▚▖▄▄▄▄     ■  ▐▌   ▄▄▄▄ '
+        '\033[36m    █ ▗▞▀▚▖▄▄▄▄     ■  ▐▌   ▄▄▄▄ '
         , '    █ ▐▛▀▀▘█   █ ▗▄▟▙▄▖▐▌      █ '
         , '    █ ▝▚▄▄▖█   █   ▐▌  ▐▛▀▚▖█▀▀▀ '
         , ' ▗▄▖█              ▐▌  ▐▌ ▐▌█▄▄▄ '
-        , '▐▌ ▐▌   By:        ▐▌            '
-        , ' ▝▀▜▌       Ali Aryani (Hannibal)'
-        , '▐▙▄▞▘   github.com/thehannibalist'
+        , '▐▌ ▐▌   \033[32mBy:\033[36m        ▐▌            '
+        , ' ▝▀▜▌       \033[32mAli Aryani (Hannibal)\033[36m'
+        , '▐▙▄▞▘     \033[32mhttps://git.new/dQjjC0r\033[36m'
     ]
     for line in logo :
         print ('\033[36m',line)
+        sleep(.2)
     print('\033[39m')
     sleep (1)
     
