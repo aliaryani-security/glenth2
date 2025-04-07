e81760feb7097428e1a81787321126fa2ae80177
Hannibal <lenesis@proton.me>
2025-04-07 16:03:47 +0330

    function fully defined

--- modules/find_vid.py
+++ modules/find_vid.py
@@ -1,6 +1,13 @@
 import pathlib
 
-p = pathlib.Path("/media/thehannibalist/NewF")
-files = list(p.glob("*.mkv"))
-for f in files:
-    print (f)
+def search(path:str,sub_dir:bool,file_types:list) -> list :
+    p = pathlib.Path(path) #path to scan
+    files = [] # empty list defined
+    for t in file_types: # scan for different file types
+        if sub_dir : # to scan sub directories as well
+            for file in p.glob(f"**/*.{t}"):
+                files.append(file) # append file to the list
+        else : # do not scan sub directories
+            for file in p.glob(f"*.{t}"):
+                files.append(file)
+    return files
