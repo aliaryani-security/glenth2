01b139a3dc47182311cd8a9754af78c1bfd5334a
Ali Aryani <lenesis@proton.me>
2025-04-24 11:53:14 +0330

    update banner

--- modules/banner.py
+++ modules/banner.py
@@ -7,8 +7,8 @@ def show():
         , '    █ ▝▚▄▄▖█   █   ▐▌  ▐▛▀▚▖█▀▀▀ '
         , ' ▗▄▖█              ▐▌  ▐▌ ▐▌█▄▄▄ '
         , '▐▌ ▐▌   \033[32mBy:\033[36m        ▐▌            '
-        , ' ▝▀▜▌       \033[32mAli Aryani (Hannibal)\033[36m'
-        , '▐▙▄▞▘     \033[32mhttps://git.new/dQjjC0r\033[36m'
+        , ' ▝▀▜▌       \033[32mAli Aryani\033[36m'
+        , '▐▙▄▞  ▘\033[32mgithub: aliaryani-security\033[36m'
     ]
     for line in logo :
         print ('\033[36m',line)
