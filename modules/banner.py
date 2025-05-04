881e257991ab1026ba0a7b1b9f001cfa79b98dab
Ali Aryani <lenesis@proton.me>
2025-05-04 19:16:55 +0330

    fix banner

--- modules/banner.py
+++ modules/banner.py
@@ -8,7 +8,7 @@ def show():
         , ' ▗▄▖█              ▐▌  ▐▌ ▐▌█▄▄▄ '
         , '▐▌ ▐▌   \033[32mBy:\033[36m        ▐▌            '
         , ' ▝▀▜▌       \033[32mAli Aryani\033[36m'
-        , '▐▙▄▞  ▘\033[32mgithub: aliaryani-security\033[36m'
+        , '▐▙▄▞▘  \033[32mgithub: aliaryani-security\033[36m'
     ]
     for line in logo :
         print ('\033[36m',line)
